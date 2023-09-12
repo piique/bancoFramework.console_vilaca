f746077c257949bb1633299f1ebf4e2476726486
Pedro Vilaça <pedrovilacav@gmail.com>
2023-09-11 22:52:13 -0300

    Criado menu de saque e deposito

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -9,25 +9,52 @@ internal class Program
         Console.WriteLine("Por favor, identifique-se");
         Console.WriteLine("");
         var pessoa = Identificacao();
+        Menu(pessoa);
     }
 
     static Pessoa Identificacao()
     {
         var pessoa = new Pessoa();
 
-        Console.WriteLine("Seu número de identificação:");
-        pessoa.Id = int.Parse(Console.ReadLine());
+        Console.Write("Seu número de identificação: ");
+        pessoa.Id = int.Parse(s: Console.ReadLine() ?? "");
 
-        Console.WriteLine("Seu nome:");
-        pessoa.Nome = Console.ReadLine();
+        Console.Write("Seu nome: ");
+        pessoa.Nome = Console.ReadLine() ?? "";
 
-        Console.WriteLine("Seu CPF:");
-        pessoa.Cpf = Console.ReadLine();
+        Console.Write("Seu CPF: ");
+        pessoa.Cpf = Console.ReadLine() ?? "";
         Console.Clear();
 
-        Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        Console.ReadKey();
-        
         return pessoa;
+    }
+
+    static void Menu(Pessoa pessoa)
+    {
+        string? opcao;
+        do
+        {
+            Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
+            Console.WriteLine("1 - Depósito");
+            Console.WriteLine("2 - Saque");
+            Console.WriteLine("3 - Sair");
+            Console.Write("Opção: ");
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Depósito");
+                    break;
+                case "2":
+                    Console.WriteLine("Saque");
+                    break;
+                case "3":
+                    break;
+                default:
+                    Console.Clear();
+                    break;
+            }
+        } while (opcao != "3");
     }
 }
